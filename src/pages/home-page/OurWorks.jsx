420274f7d6b6def546c90d812a1124d69efdf4ed
Ando Avetisyan <andoavetis0127@gmail.com>
2022-09-22 18:36:58 +0400

    ZoomIn icon is placed

--- src/pages/home-page/OurWorks.jsx
+++ src/pages/home-page/OurWorks.jsx
@@ -1,19 +1,55 @@
 //Mui components
-import { Grid, Typography, ImageList, ImageListItem } from "@mui/material";
+import { Grid, Typography, Box, Button, IconButton } from "@mui/material";
 //react router
 import { NavLink } from "react-router-dom";
 //react components
 import { CommonBox } from "src/components/common";
+//Mui icons
+import ZoomInIcon from "@mui/icons-material/ZoomIn";
+//react hooks
+import { useState } from "react";
 
 export default function OurWorks({ works }) {
+  const [hover, setHover] = useState(false);
+  const showIcon = () => {
+    setHover(true);
+  };
   return (
     <CommonBox title="НАШИ РАБОТЫ">
       <Grid container spacing={8}>
         <Grid item>
           <Grid container spacing={3}>
             {works?.slice(0, 6).map(({ image, id, subtitle }) => (
-              <Grid item sx={{ cursor: "pointer" }} key={id}>
-                <img src={image} alt={subtitle} />
+              <Grid
+                item
+                sx={{ cursor: "pointer" }}
+                key={id}
+                onMouseEnter={() => showIcon()}
+                onMouseLeave={() => setHover(false)}
+              >
+                <Box position="relative">
+                  <img src={image} alt={subtitle} />
+                  {hover ? (
+                    <Button
+                      sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        background: "rgba(0, 0, 0, 0.7)",
+                        borderRadius: "5px",
+                        width: "40px",
+                        height: "40px",
+                        "&:hover": { background: "rgba(0, 0, 0, 0.7)" }
+                      }}
+                    >
+                      <IconButton>
+                        <ZoomInIcon sx={{ color: "white", width: "20px", height: "20px" }} />
+                      </IconButton>
+                    </Button>
+                  ) : (
+                    ""
+                  )}
+                </Box>
               </Grid>
             ))}
           </Grid>
